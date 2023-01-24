6560ea743484217d605169b2bcc84d3db585caa2
mideactive <jenyoolumide@yahoo.com>
2023-01-24 22:03:09 +0000

    sort heap

--- sort.h
+++ sort.h
@@ -38,6 +38,7 @@ void counting_sort(int *array, size_t size);
 void merge_ar(int *subarr, int *buff, size_t front, size_t mid, size_t back);
 void merge_sort_recursive(int *subarr, int *buff, size_t front, size_t back);
 void merge_sort(int *array, size_t size);
-
+void max_heapify(int *array, size_t size, size_t base, size_t root);
+void heap_sort(int *array, size_t size);
 
 #endif/*SORT_H*/
